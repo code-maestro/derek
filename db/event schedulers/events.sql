0c89e84430013e72c8322ae25a6fd66353113919
code-maestro <michaelajnew@gmail.com>
2023-02-28 23:12:39 +0300

    newborns development

--- db/event schedulers/events.sql
+++ db/event schedulers/events.sql
@@ -6,23 +6,15 @@ CREATE EVENT IF NOT EXISTS animals_due_tracker
 
     ON SCHEDULE
 
-        EVERY 1 DAY
+        EVERY  1 DAY
 
     COMMENT 'EXPECTING ANIMALS DELIVERY TRACKER'
 
     DO
     
         BEGIN
-            -- 2. Get the required data
-            SET @animal_id = (SELECT animal_id FROM due_dates WHERE delivery_date = CURDATE());
-            SET @parent_tag = (SELECT animal_tag FROM animal WHERE id = @animal_id);
-            SET @farma_id = (SELECT farma_id FROM animal WHERE id = @animal_id);
-            SET @new_max_id = (SELECT MAX(id) FROM animal WHERE farma_id = @farma_id);
-            SET @new_born_tag = (SELECT CONCAT(UPPER(animal_type),'-',(@new_max_id +1)) FROM animal WHERE id = @animal_id);
-
-            -- 3. insert into new_born table
-            INSERT INTO new_born(new_born_tag, parent_id, dob)
-            VALUES(@new_born_tag, @animal_id, CURDATE());
+           
+           CALL getNewBorns();
 
         END |
 
